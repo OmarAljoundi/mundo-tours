a08360205a4d80a7e54950007638d2bb1f8beb6d
Omar Aljoundi <omaraljoundi@gmail.com>
2024-01-14 21:59:18 +0300

    .

--- app/server-sitemap.xml/route.ts
+++ app/server-sitemap.xml/route.ts
@@ -1,7 +1,12 @@
-import { getAllPaths } from "@/lib/sitemapHelper";
-import { getServerSideSitemap } from "next-sitemap";
+import { getAllPaths } from '@/lib/sitemapHelper'
+import { getServerSideSitemap } from 'next-sitemap'
+import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  const urls = await getAllPaths();
-  return getServerSideSitemap(urls);
+  // const urls = await getAllPaths();
+  // return getServerSideSitemap(urls);
+
+  return NextResponse.json({
+    success: true,
+  })
 }
